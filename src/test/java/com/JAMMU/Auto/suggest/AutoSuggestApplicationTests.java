331d37e35aed7942894fdbcddd3bfb61980a26f3
BALA AKSHAY <balaakshay789@gmail.com>
2026-07-17 14:57:09 +0530

    html1

---
+++ src/test/java/com/JAMMU/Auto/suggest/AutoSuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JAMMU.Auto.suggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
